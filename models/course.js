b3571ee51c48cadec96503bed71e6726b5919a9a
Thurston Daly <dalythu@gmail.com>
2021-09-29 15:25:41 -0400

    Defined Course/User Models

---
+++ models/course.js
@@ -0,0 +1,54 @@
+'use strict';
+const { Model, DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+  class Course extends Model {}
+  Course.init({
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'A title is required'
+        }, 
+        notEmpty: {
+          msg: 'Please provide a course name'
+        }
+      }
+    },
+    description: {
+      type: DataTypes.TEXT,
+      validate: {
+        notNull: {
+          msg: 'A course description is required'
+        }, 
+        notEmpty: {
+          msg: 'Please provide a course description'
+        }
+      }
+    },
+    estimatedTime: {
+        type: DataTypes.STRING,
+        validate: {
+          notNull: {
+            msg: 'An estimated time is are required'
+          }, 
+          notEmpty: {
+            msg: 'Please provide an estimated time'
+          }
+        }
+      },
+  }, { sequelize });
+
+  Course.associate = (models) => {  
+    Course.belongsTo(models.User, { //Each course only has one user
+      as: 'user', //allias - matches user model 
+      foreignKey: { 
+        fieldName: 'userId',
+        allowNull: false,
+      },
+    }); 
+  };
+
+  return Course;
+};
---
+++ models/user.js
@@ -0,0 +1,76 @@
+'use strict';
+const { Model, DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+  class User extends Model {}
+  User.init({
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false, 
+      validate: {
+        notNull: {
+          msg: 'A first name is required'
+        }, 
+        notEmpty: {
+          msg: 'Please provide a first name'
+        }
+      }
+    },
+    lastName: {
+        type: DataTypes.STRING,
+        allowNull: false, 
+        validate: {
+          notNull: {
+            msg: 'A last name is required'
+          }, 
+          notEmpty: {
+            msg: 'Please provide a last name'
+          }
+        }
+    },
+    emailAddress: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: {
+        msg: 'The email you entered already exists' //constraint that prevents the same email address from being entered twice
+      },
+      validate: {
+        notNull: {
+          msg: 'An email is required'
+        }, 
+        isEmail: {
+          msg: 'Please provide a valid email'
+        }
+      }    
+    },
+    password: {
+      type: DataTypes.STRING, 
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'A password is required'
+        }, 
+        notEmpty: {
+          msg: 'Please provide a password'
+        }, 
+        len: {
+          args: [8, 20], //array signifying required length 
+          msg: 'The password should be between 8 and 20 characters in length'
+        }
+      }
+    },
+  }, { sequelize });
+
+  // Associations between course and user models 
+  User.associate = (models) => { //tells Sequelize that a user can be associated with one or more courses
+    User.hasMany(models.Course, {
+      as: 'user', 
+      foreignKey: {
+        fieldName: 'userId',
+        allowNull: false,
+      },
+    });
+  };
+
+  return User;
+};
